d4b80736875557ce298e59c4873e2a051385450c
Prash Mohan <prashmohanuon@gmail.com>
2021-08-31 20:10:09 +0100

    added footballer class

---
+++ CSharpSandbox/Footballer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharpSandbox
+{
+    public class Footballer
+    {
+     public string Name { get; set; }
+     public string Club { get; set; }
+     public int Value { get; set; }
+
+    public Footballer(string name, string club, int value)
+    {
+            this.Name = name;
+            this.Club = club;
+            this.Value = value;
+    }
+    }
+}
--- CSharpSandbox/Program.cs
+++ CSharpSandbox/Program.cs
@@ -7,6 +7,11 @@ namespace CSharpSandbox
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Prash!");
+
+            Footballer Ronaldo = new Footballer("Ronaldo", "Manchester United", 25000000);
+
+            Console.WriteLine(Ronaldo.Club);
+            Console.WriteLine($"{Ronaldo.Name} plays for {Ronaldo.Club} and is worth {Ronaldo.Value + 10000000}");
         }
     }
 }
