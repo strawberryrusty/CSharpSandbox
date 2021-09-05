3a1d56fe37541aa13b75b21972bf25b75e807a45
Prash Mohan <prashmohanuon@gmail.com>
2021-09-05 21:52:57 +0100

    string interpolation

--- CSharpSandbox/Program.cs
+++ CSharpSandbox/Program.cs
@@ -6,12 +6,13 @@ namespace CSharpSandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Prash!");
 
             Footballer Ronaldo = new Footballer("Ronaldo", "Manchester United", 25000000);
+            Footballer VanPersie = new Footballer("Van Persie", "Arsenal", 130000);
 
             Console.WriteLine(Ronaldo.Club);
-            Console.WriteLine($"{Ronaldo.Name} plays for {Ronaldo.Club} and is worth {Ronaldo.Value + 10000000}");
+            Console.WriteLine($"{VanPersie.Name} plays for {Ronaldo.Club} and is worth {Ronaldo.Value + 10000000}");
         }
     }
 }
